9d8f227f496c3fbb0ad024db8ce9a1252732a7e5
pwomotepawla-hub <pwomotepawla@gmail.com>
2026-05-31 12:33:23 -0400

    test: corregir importacion pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from app import app
 
 def test_home():
     client = app.test_client()
     response = client.get("/")
+    
     assert response.status_code == 200
+    assert b"Microservicio DevOps funcionando" in response.data
